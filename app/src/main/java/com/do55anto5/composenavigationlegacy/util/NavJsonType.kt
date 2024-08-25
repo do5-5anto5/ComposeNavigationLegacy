5f27150fb714f56586d1389dc57893ffba03dcdc
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-08-25 13:14:07 -0300

    Feat: Implement NavJsonType.kt   - this Kotlin file have extensions that convert Any type to Json String, and do reverse too.

---
+++ app/src/main/java/com/do55anto5/composenavigationlegacy/util/NavJsonType.kt
@@ -0,0 +1,11 @@
+package com.do55anto5.composenavigationlegacy.util
+
+import com.google.gson.Gson
+
+fun Any.toJson(): String {
+    return Gson().toJson(this)
+}
+
+inline fun <reified T> String.fromJson(): T {
+    return Gson().fromJson(this, T::class.java)
+}
